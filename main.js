d93c68378e2d5d07a3f07e700f6ad1ca8ceb1695
Christian Helvin <christian@ignitehere.com>
2018-03-13 10:57:57 -0500

    fixed issue #4 for app closing

--- main.js
+++ main.js
@@ -330,6 +330,10 @@ app.on('window-all-closed', function () {
   }
 })
 
+app.on('before-quit', function () {
+  mainWindow.destroy()
+})
+
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
